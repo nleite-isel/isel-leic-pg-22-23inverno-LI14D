9a6c1964135ba8524bbd8da1954f7008c00e94bb
Nuno Leite <nleite@cc.isel.ipl.pt>
2022-09-22 18:24:00 +0100

    Aula 02-03

---
+++ Semana 1/Aula 02-03/ReadLine.kt
@@ -0,0 +1,18 @@
+
+
+fun main() {
+
+/*
+	println("Introduza uma linha de texto")
+	val str: String = readln()
+	
+	// Print val contents
+	println("Texto introduzido $str")
+*/	
+	println("Introduza um inteiro:")
+	val number: Int = readln().toInt()
+	
+	// Print val contents
+	println("Numero introduzido: $number")
+
+}
